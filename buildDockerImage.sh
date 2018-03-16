996974e9724d9cf7204bffad972b48d9fffddd65
wzh <wangzheng422@foxmail.com>
2018-03-16 18:51:24 +0800

    alauda

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 modules=( admin-dashboard hystrix-dashboard service-a service-b zuul )
 
 for module in "${modules[@]}"; do
-    docker build -t "spring-cloud-consul-example/${module}:latest" ${module}
+    docker build -t "10.11.0.8:5000/spring-cloud-consul-example-${module}:latest" ${module}
+    docker push "10.11.0.8:5000/spring-cloud-consul-example-${module}:latest"
 done

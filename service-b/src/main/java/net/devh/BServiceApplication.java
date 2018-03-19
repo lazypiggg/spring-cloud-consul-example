2eca6b1b172f260d64d6f55431a392100561d390
wzh <wangzheng422@foxmail.com>
2018-03-19 16:45:59 +0800

    swagger

--- service-b/src/main/java/net/devh/BServiceApplication.java
+++ service-b/src/main/java/net/devh/BServiceApplication.java
@@ -2,20 +2,19 @@ package net.devh;
 
 import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
 
-import java.util.ArrayList;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.annotations.Api;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -36,10 +35,22 @@ public class BServiceApplication {
                 .build()
                 .pathMapping("/")
                 .useDefaultResponseMessages(false)
-                .apiInfo(new ApiInfo("Service B API Doc", "Service B API Doc", "1.0", "https://github.com/wangzheng422/spring-cloud-consul-example",
-                        new Contact("George", "https://github.com/wangzheng422", "wangzheng422@gmail.com"), "", "", new ArrayList<VendorExtension>()))
+                .apiInfo(
+                    new ApiInfoBuilder().title("Service B API Doc")
+                    .description("Service B API Doc")
+                    .version("1.0")
+                    .termsOfServiceUrl("https://github.com/wangzheng422/spring-cloud-consul-example")
+                    .contact(new Contact("George", "https://github.com/wangzheng422", "wangzheng422@gmail.com"))
+                    .build())
                 .forCodeGeneration(true);
+
     }
+
+    @Bean
+	UiConfiguration uiConfig() {
+		       
+        return UiConfigurationBuilder.builder().build();
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(BServiceApplication.class, args);
